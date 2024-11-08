0e61896af3dbbc8adde9cab5fadb01bd53d28aa0
Back-End.07 <sonita.borin2@gmail.com>
2024-11-08 15:00:53 +0700

    modified:   ColorAdmin/Data/ApplicationDbContext.cs

--- ColorAdmin/Data/ApplicationDbContext.cs
+++ ColorAdmin/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace ColorAdmin.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
         }
     }
 }
